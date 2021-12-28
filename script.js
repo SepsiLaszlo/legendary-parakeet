854aac9f79e9ac488a8324c57af9eea9fcccb942
SepsiLaszlo <sepsi.laszlo99@gmail.com>
2021-12-28 11:54:07 +0100

    implement rubicks cube

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("Hello")
+const colors = {
+    "blue":"white",
+    "white":"green",
+    "green":"red",
+    "red":"yellow",
+    "yellow":"orange",
+    "orange":"blue"
+}
+const tileElments = [...document.getElementsByClassName("tile")]
+const tiles = tileElments.map(function(te){ return {element: te, color: "blue"}})
+tileElments.forEach(elem => elem.addEventListener("click",changeTileColor))
+
+function changeTileColor(event){
+    const tile = tiles.find(tile => tile.element == event.target)
+    tile.color = colors[tile.color]
+    tile.element.style.backgroundColor = tile.color
+}
+
